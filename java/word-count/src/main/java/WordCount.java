209523fcbf4b3789716fc74f0599004b1b71b461
evanotero <oteroev@bc.edu>
2016-09-11 17:40:41 -0400

    Completed word-count

--- java/word-count/src/main/java/WordCount.java
+++ java/word-count/src/main/java/WordCount.java
@@ -1,2 +1,12 @@
+import java.util.*;
+
 public class WordCount {
+    public Map<String, Integer> phrase (String str) {
+        String[] words = str.split("\\W+");
+        Map<String, Integer> counts = new HashMap<>();
+
+        for (String word : words)
+            counts.merge(word.toLowerCase(), 1, (a, b) -> a + b);
+        return counts;
+    }
 }
