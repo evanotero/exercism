a056996ed2cbf59e6a2bccda95c08f9b8b4d2282
evanotero <oteroev@bc.edu>
2016-09-11 19:17:59 -0400

    Anagram is now more efficient

--- java/anagram/src/main/java/Anagram.java
+++ java/anagram/src/main/java/Anagram.java
@@ -1,31 +1,26 @@
+import java.util.Arrays;
 import java.util.List;
 import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.Map;
 
 
 public class Anagram {
     private String word;
-    private Map<Character, Integer> wordmap = new HashMap<>();
+    private String sorted;
 
     public Anagram (String s) {
         word = s.toLowerCase();
+        sorted = sort(word);
+    }
 
-        int length = word.length();
-        for (int i = 0; i < length; i++)
-            wordmap.merge(word.charAt(i), 1, (a, b) -> a + b);
+    private String sort(String s) {
+        char[] chars = s.toCharArray();
+        Arrays.sort(chars);
+        return new String(chars);
     }
 
     private boolean isAnagram(String other) {
-        boolean result = false;
-        Map<Character, Integer> othermap = new HashMap<>();
         other = other.toLowerCase();
-
-        int length = other.length();
-        if (length == word.length())
-            for (int i = 0; i < length; i++)
-                othermap.merge(other.charAt(i), 1, (a, b) -> a + b);
-        return othermap.equals(wordmap) && !other.equals(word.toLowerCase());
+        return !other.equals(word) && sort(other).equals(sorted);
     }
 
     public List<String> match(List<String> words) {
